17ae5000feb5a302b61356f4f2152db9059c339b
ArtemMikhailenko <Artemlend@gmail.com>
2025-12-10 18:23:29 +0200

    fix: add Module dependency to ProgressService

--- src/progress/progress.module.ts
+++ src/progress/progress.module.ts
@@ -3,10 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ProgressController } from './progress.controller';
 import { ProgressService } from './progress.service';
 import { User, UserSchema } from '../schemas/user.schema';
+import { Module as ModuleSchema, ModuleSchema as ModuleSchemaDefinition } from '../schemas/module.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: ModuleSchema.name, schema: ModuleSchemaDefinition },
+    ]),
   ],
   controllers: [ProgressController],
   providers: [ProgressService],
--- src/progress/progress.service.ts
+++ src/progress/progress.service.ts
@@ -2,12 +2,14 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from '../schemas/user.schema';
+import { Module, ModuleDocument } from '../schemas/module.schema';
 import { getAvatarForLevel, getLevelByCompletedModules } from '../config/avatars.config';
 
 @Injectable()
 export class ProgressService {
   constructor(
     @InjectModel(User.name) private userModel: Model<UserDocument>,
+    @InjectModel(Module.name) private moduleModel: Model<ModuleDocument>,
   ) {}
 
   async completeLesson(
