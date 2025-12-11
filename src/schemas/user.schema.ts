96735370e08c07b9907f158412625b6004f34369
ArtemMikhailenko <Artemlend@gmail.com>
2025-12-11 02:33:33 +0200

    delete student letter create

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -14,8 +14,8 @@ export class User {
   @Prop({ required: true })
   firstName: string;
 
-  @Prop({ required: true })
-  lastName: string;
+  @Prop()
+  lastName?: string;
 
   @Prop()
   phoneOrTelegram?: string; // Номер телефону або телеграм
