37b66a1dd0be6cd5aedef1759c6b8f3ab871cce1
ArtemMikhailenko <Artemlend@gmail.com>
2025-12-11 02:57:09 +0200

    add voice comment

--- src/config/multer.config.ts
+++ src/config/multer.config.ts
@@ -17,3 +17,23 @@ export const imageFileFilter = (req, file, callback) => {
   }
   callback(null, true);
 };
+
+export const audioFileFilter = (req, file, callback) => {
+  if (!file.originalname.match(/\.(mp3|wav|webm|ogg|m4a)$/)) {
+    return callback(new Error('Тільки аудіо файли дозволені!'), false);
+  }
+  callback(null, true);
+};
+
+export const multerConfig = {
+  storage: diskStorage({
+    destination: './uploads/audio-feedback',
+    filename: (req, file, callback) => {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+      const ext = extname(file.originalname);
+      const filename = `audio-${uniqueSuffix}${ext}`;
+      callback(null, filename);
+    },
+  }),
+  fileFilter: audioFileFilter,
+};
